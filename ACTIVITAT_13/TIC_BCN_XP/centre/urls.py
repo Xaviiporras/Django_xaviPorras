bb207213d4bc1f968cf965f5ae71f2c6309e19ec
Xavi Porras <2023_xavier.porras@iticbcn.cat>
2025-01-27 19:01:00 +0100

    tercera clase

--- ACTIVITAT_13/TIC_BCN_XP/centre/urls.py
+++ ACTIVITAT_13/TIC_BCN_XP/centre/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('proff',views.proff, name='proff'),
+    path('users',views.users, name='users')
 ]
--- ACTIVITAT_13/TIC_BCN_XP/centre/views.py
+++ ACTIVITAT_13/TIC_BCN_XP/centre/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-def index(request) :
-    return HttpResponse("Hello, world")
+def proff(request):
+    prof = {"name": "Roger", "surname": "Sobrino", "age": "23"}
+    return render(request, 'proff.html', {'nom': prof["name"], 'cognom': prof["surname"], 'edat': prof["age"]})
+
+def users(request):
+    alum = {"name": "Oriol", "surname": "Roca", "age": "18"}
+    return render(request, 'users.html', {'alumn': alum})
 
 # Create your views here.
