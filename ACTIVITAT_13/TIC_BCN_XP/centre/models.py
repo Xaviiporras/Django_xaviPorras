1079c818e3b1b540566aea74b217f0360c16ac8a
Xavi Porras <2023_xavier.porras@iticbcn.cat>
2025-02-02 01:00:33 +0100

    Exercici3 fet

--- ACTIVITAT_13/TIC_BCN_XP/centre/models.py
+++ ACTIVITAT_13/TIC_BCN_XP/centre/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    nom = models.CharField(max_length=100)
+    cognom1 = models.CharField(max_length=100)
+    cognom2 = models.CharField(max_length=100, blank=True, null=True)
+    correu = models.EmailField(unique=True)
+    cursos = models.TextField(blank=True, null=True)  
+    moduls = models.TextField(blank=True, null=True)  
+    edat = models.IntegerField(blank=True, null=True)  
+    rol = models.CharField(max_length=20)
