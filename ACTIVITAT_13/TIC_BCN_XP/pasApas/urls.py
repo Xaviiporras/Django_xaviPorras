5a0a2a5b942f96bbb305f1da68a03a864b1d6383
Xavi Porras <2023_xavier.porras@iticbcn.cat>
2025-02-10 17:02:33 +0100

    pasApas fet

--- ACTIVITAT_13/TIC_BCN_XP/pasApas/urls.py
+++ ACTIVITAT_13/TIC_BCN_XP/pasApas/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('guardar_sesion',views.guardar_sesion, name='guardar_sesion'),
     path('recuperar_sesion',views.recuperar_sesion, name='recuperar_sesion'),
-    path('eliminar_sesion',views.eliminar_sesion, name='eliminar_sesionn'),
+    path('eliminar_sesion',views.eliminar_sesion, name='eliminar_sesion'),
 ]
